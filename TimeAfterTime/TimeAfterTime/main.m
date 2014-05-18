e848a89e77155e58b6e051e086fb607a7745513b
Thiago Brandão <thiagobrandam@gmail.com>
2014-05-18 11:34:41 -0300

    Challenge: How Many Seconds I’ve been ALive

--- TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -12,9 +12,25 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+//        NSDate *now = [NSDate date];
+        NSDate *now = [[NSDate alloc] init];
+        NSLog(@"This NSDate object lives at %p", now);
+        NSLog(@"The date is %@", now);
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It's been %f seconds since the start of 1970", seconds);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100000 seconds it will be %@", later);
+        
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        NSLog(@"My calendar is %@",[cal calendarIdentifier]);
+        
+        unsigned long day = [cal ordinalityOfUnit:NSDayCalendarUnit
+                                           inUnit:NSMonthCalendarUnit
+                                          forDate:now];
+        
+        NSLog(@"This is day %lu of the month",day);
         
     }
     return 0;
