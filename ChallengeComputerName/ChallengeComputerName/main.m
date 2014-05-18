6c0bfe81ce53c13785c530281314c32ea1c60eb8
Thiago Brandão <thiagobrandam@gmail.com>
2014-05-18 10:43:36 -0300

    TimeAfterTime and challenge!

---
+++ ChallengeComputerName/ChallengeComputerName/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ChallengeComputerName
+//
+//  Created by thiago on 5/18/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
